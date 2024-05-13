3ce4be9e4418ce294035f70ae537b8cb1d80d203
Ali El-Mallah <131396543+AliAmr527@users.noreply.github.com>
2024-05-13 21:49:17 +0300

    adding more routes

--- courses/src/modules/courses/courses.router.js
+++ courses/src/modules/courses/courses.router.js
@@ -4,12 +4,13 @@ import { Router } from "express"
 
 const router = Router()
 
-router.get("/getAllCourses", CC.getAllCourses)
 router.post("/create", checkRole("instructor"), CC.createCourse)
 router.get("/getAllCourses", CC.getAllCourses)
 router.get("/coursesForInstructor",checkRole("instructor"), CC.getCoursesForInstructor)
 router.post("/apply", CC.applyCourse)
 router.post("/cancel", CC.cancelCourseEnrollment)
+router.post("/accept", CC.acceptRequest)
+router.post("/reject", CC.rejectRequest)
 router.post("/review", CC.reviewCourse)
 
 export default router
