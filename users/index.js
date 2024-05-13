b85cc0c852317606595af9c5f0e8e1798d5d8560
Ali El-Mallah <131396543+AliAmr527@users.noreply.github.com>
2024-05-13 19:36:04 +0300

    adding cors

--- users/index.js
+++ users/index.js
@@ -1,9 +1,16 @@
-import express from 'express'
-import bootstrap from './src/index.router.js'
+import express from "express"
+import bootstrap from "./src/index.router.js"
+import cors from "cors"
 const app = express()
 const port = 5001
 
-bootstrap(app,express)
+var corsOptions = {
+	origin: "*",
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions))
+
+bootstrap(app, express)
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 export default server
