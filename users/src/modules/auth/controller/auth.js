02e3a204c9c683a2da84e5b863c8979b89292795
Ali El-Mallah <131396543+AliAmr527@users.noreply.github.com>
2024-05-13 20:44:35 +0300

    fixing tabs 2

--- users/src/modules/auth/controller/auth.js
+++ users/src/modules/auth/controller/auth.js
@@ -5,12 +5,12 @@ export const register = async (req, res) => {
 	try {
 		const isExist = await User.findOne({ email })
 		if (isExist) return res.status(400).send({ message: "User already exist!" })
-		const user = await User.create({ name, email, password, affiliation, yearsOfExperience, bio,role })
+		const user = await User.create({ name, email, password, affiliation, yearsOfExperience, bio, role })
 		const output = {
-            id:user._id,
-            role:user.role
-        }
-        res.status(200).json({ output })
+			id: user._id,
+			role: user.role,
+		}
+		res.status(200).json({ output })
 	} catch (error) {
 		res.status(400).json({ message: error.message })
 	}
@@ -26,11 +26,11 @@ export const login = async (req, res) => {
 		if (user.password !== password) {
 			return res.status(400).send({ message: "Incorrect password!" })
 		}
-        const output = {
-			id:user._id,
-			role:user.role,
-			name:user.name
-        }
+		const output = {
+			id: user._id,
+			role: user.role,
+			name: user.name,
+		}
 		res.status(200).json({ output })
 	} catch (error) {
 		res.status(400).json({ message: error.message })
@@ -39,7 +39,7 @@ export const login = async (req, res) => {
 
 export const checkUser = async (req, res) => {
 	const user = await User.findById(req.params.id)
-	if(!user){
+	if (!user) {
 		return res.status(200).send("doesn't exist")
 	}
 	return res.status(200).send("exists")
