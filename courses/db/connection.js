1226717075daf9dcc14fd5fadc13e4f67ad3d332
Ali El-Mallah <131396543+AliAmr527@users.noreply.github.com>
2024-05-13 19:26:01 +0300

    updating connection strings to atlas remote database

--- courses/db/connection.js
+++ courses/db/connection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const dbConnection = () => mongoose.connect("mongodb://localhost:27017/microservices_courses")
+const dbConnection = () => mongoose.connect("mongodb+srv://reaper9027:ZYfH3T09JcNGb8MR@cluster0.vreqasy.mongodb.net/courses?retryWrites=true&w=majority&appName=Cluster0")
 	.then(() => console.log("Connected!"))
 	.catch((e) => {
 		console.log(e.message)
