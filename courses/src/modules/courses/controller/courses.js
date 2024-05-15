b797bc0554938c752910c320d753938e91d028b6
Ali El-Mallah <131396543+AliAmr527@users.noreply.github.com>
2024-05-15 16:49:07 +0300

    adding more output in viewCurrentAndPastCourses

--- courses/src/modules/courses/controller/courses.js
+++ courses/src/modules/courses/controller/courses.js
@@ -24,7 +24,7 @@ export const createCourse = async (req, res) => {
 
 export const getCourses = async (req, res) => {
 	try {
-		const courses = await courseModel.find({},{__v:0})
+		const courses = await courseModel.find({}, { __v: 0 })
 		let obj = JSON.parse(JSON.stringify(courses))
 		obj.forEach((course) => {
 			course.requestedStudents = course.requestedStudents.length
@@ -34,7 +34,7 @@ export const getCourses = async (req, res) => {
 				review._id = undefined
 			})
 		})
-		return res.json({courses:obj})
+		return res.json({ courses: obj })
 	} catch (error) {
 		return res.status(400).json({ message: error.message })
 	}
@@ -60,12 +60,12 @@ export const deleteCourse = async (req, res) => {
 }
 
 export const getAllCourses = async (req, res) => {
-	let reqQuery = courseModel.find({published:true})
+	let reqQuery = courseModel.find({ published: true })
 	if (req.query.sort == "name") {
 		reqQuery.sort({ name: 1 })
 	}
 	if (req.query.sort == "rating") {
-		reqQuery.sort({ rating: -1 , name:1})
+		reqQuery.sort({ rating: -1, name: 1 })
 	}
 	if (req.query.name) {
 		reqQuery.find({ name: { $regex: req.query.name || " ", $options: "i" } })
@@ -89,15 +89,15 @@ export const getAllCourses = async (req, res) => {
 export const getCoursesForInstructor = async (req, res) => {
 	const isExist = checkUser(req.body.id)
 	if (isExist.data == "doesn't exist") return res.status(404).send("instructor not found")
-	const courses = await courseModel.find({ createdBy: req.body.id,published:true }, { _id: 1, name: 1, requestedStudents: 1 })
+	const courses = await courseModel.find({ createdBy: req.body.id, published: true }, { _id: 1, name: 1, requestedStudents: 1 })
 	if (!courses) return res.status(404).send("no courses found")
 	const output = JSON.parse(JSON.stringify(courses))
 	for (let i = output.length - 1; i >= 0; i--) {
 		if (output[i].requestedStudents.length === 0) {
-			output.splice(i, 1);
+			output.splice(i, 1)
 		} else {
 			for (let j = 0; j < output[i].requestedStudents.length; j++) {
-				output[i].requestedStudents[j]._id = output[i].requestedStudents[j]._id.toString();
+				output[i].requestedStudents[j]._id = output[i].requestedStudents[j]._id.toString()
 			}
 		}
 	}
@@ -114,7 +114,11 @@ export const applyCourse = async (req, res) => {
 		$or: [{ enrolledStudents: { $elemMatch: { _id: studentId, name: name } } }, { pastStudents: { $elemMatch: { _id: studentId, name: name } } }],
 	})
 	if (isExist) return res.status(409).send("you already are enrolled for this course or have taken this course before")
-	const course = await courseModel.updateOne({ _id: courseId,published:true }, { $addToSet: { requestedStudents: { _id: studentId, name: name } } }, { new: true })
+	const course = await courseModel.updateOne(
+		{ _id: courseId, published: true },
+		{ $addToSet: { requestedStudents: { _id: studentId, name: name } } },
+		{ new: true }
+	)
 	if (!course.modifiedCount) {
 		return res.status(409).send("you already applied for this course")
 	}
@@ -128,7 +132,7 @@ export const cancelCourseEnrollment = async (req, res) => {
 	const { courseId, name, studentId } = req.body
 	const response = checkUser(studentId)
 	if (response.data == "doesn't exist") return res.status(404).send("student not found")
-	const course = await courseModel.updateOne({ _id: courseId,published:true }, { $pull: { enrolledStudents: { _id: studentId, name: name } } })
+	const course = await courseModel.updateOne({ _id: courseId, published: true }, { $pull: { enrolledStudents: { _id: studentId, name: name } } })
 	if (!course.modifiedCount) return res.status(404).send("you aren't enrolled in this course")
 	return res.status(200).send("cancelled successfully")
 }
@@ -140,7 +144,7 @@ export const acceptRequest = async (req, res) => {
 	const response = checkUser(instructorId)
 	if (response.data == "doesn't exist") return res.status(404).send("instructor not found")
 	const course = await courseModel.updateOne(
-		{ createdBy: instructorId, _id: courseId,published:true },
+		{ createdBy: instructorId, _id: courseId, published: true },
 		{ $addToSet: { enrolledStudents: { _id: studentId, name: name } } }
 	)
 	if (!course.matchedCount) return res.status(404).send("you don't own this course or course doesn't exist")
@@ -160,7 +164,7 @@ export const rejectRequest = async (req, res) => {
 	const response = checkUser(instructorId)
 	if (response.data == "doesn't exist") return res.status(404).send("instructor not found")
 	const course = await courseModel.updateOne(
-		{ createdBy: instructorId, _id: courseId,published:true },
+		{ createdBy: instructorId, _id: courseId, published: true },
 		{ $pull: { requestedStudents: { _id: studentId, name: name } } }
 	)
 	if (!course.matchedCount) return res.status(404).send("you don't own this course or this course doesn't exist")
@@ -180,7 +184,8 @@ export const rejectRequest = async (req, res) => {
 export const reviewCourse = async (req, res) => {
 	const { courseId, studentName, studentId, review, rating } = req.body
 	const reviewCourseCheck = await courseModel.findOne({
-		_id: courseId,published:true,
+		_id: courseId,
+		published: true,
 		$or: [{ enrolledStudents: { _id: studentId, name: studentName } }, { pastStudents: { _id: studentId, name: studentName } }],
 	})
 	if (!reviewCourseCheck) return res.status(404).send("you are not authorized to review this course")
@@ -203,9 +208,13 @@ export const finishedCourse = async (req, res) => {
 	return res.status(200).send("course finished")
 }
 export const viewCurrentAndPastCourses = async (req, res) => {
-	const currentCourses = await courseModel.find({ enrolledStudents: { _id: req.body.id,name:req.body.name } }, { name: 1, duration: 1, category: 1, rating: 1 })
+	const currentCourses = await courseModel.find(
+		{ enrolledStudents: { _id: req.body.id, name: req.body.name } },
+		{ _id: 1, name: 1, duration: 1, category: 1, rating: 1 }
+	)
 	const currentCoursesObj = currentCourses.map((course) => {
 		return {
+			_id: course._id,
 			name: course.name,
 			duration: course.duration,
 			category: course.category,
@@ -213,9 +222,13 @@ export const viewCurrentAndPastCourses = async (req, res) => {
 			status: "current",
 		}
 	})
-	const pastCourses = await courseModel.find({ pastStudents: { _id: req.body.id,name:req.body.name } }, { name: 1, duration: 1, category: 1, rating: 1 })
+	const pastCourses = await courseModel.find(
+		{ pastStudents: { _id: req.body.id, name: req.body.name } },
+		{ _id: 1, name: 1, duration: 1, category: 1, rating: 1 }
+	)
 	const pastCoursesObj = pastCourses.map((course) => {
 		return {
+			_id: course._id,
 			name: course.name,
 			duration: course.duration,
 			category: course.category,
@@ -234,7 +247,7 @@ export const getAllNotifications = async (req, res) => {
 
 export const getAllUnreadNotifications = async (req, res) => {
 	const count = await notificationModel.countDocuments({ studentId: req.body.id, status: "unread" })
-	if(count == 0) return res.status(404).send(false)
+	if (count == 0) return res.status(404).send(false)
 	return res.status(200).send(true)
 }
 
@@ -253,7 +266,7 @@ export const markAllAsRead = async (req, res) => {
 
 export const validateCourse = async (req, res) => {
 	const { courseId } = req.body
-	const response = await courseModel.findByIdAndUpdate(courseId,{$set:{published:true}})
+	const response = await courseModel.findByIdAndUpdate(courseId, { $set: { published: true } })
 	if (!response) return res.status(404).send("course not found")
 	return res.status(200).send("course found")
 }
