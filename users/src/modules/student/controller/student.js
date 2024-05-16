ae2c7d2c1081432a5ca1aa5ff1b7d0234683349a
Ali El-Mallah <131396543+AliAmr527@users.noreply.github.com>
2024-05-17 01:02:48 +0300

    adding years of experience null

--- users/src/modules/student/controller/student.js
+++ users/src/modules/student/controller/student.js
@@ -28,6 +28,9 @@ export const test = async (req, res) => {
 export const getStudents = async (req, res) => {
 	try {
 		const students = await userModel.find({ role: "student" },{__v:0})
+		students.forEach((student) => {
+			student.yearsOfExperience = "null"
+		})
 		return res.json({students})
 	} catch (error) {
 		return res.status(400).json({ message: error.message })
